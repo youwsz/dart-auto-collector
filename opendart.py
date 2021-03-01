51f0033ee3f9125f73f6f25c66fe624b542f1260
kisu Son <sgsco@naver.com>
2021-03-01 23:28:31 +0900

    캐시파일 수정
    Dart에서 가져온 stockcode가 ticker였음.
    ticker cache 제거 및 가져오는 코드 제거.
    공시정보 날짜로 가져오는 api 추가(연결재무제표 실적공시 가져오기위함)

--- opendart.py
+++ opendart.py
@@ -17,13 +17,12 @@ class OpenDartManager:
     __crtfcKey = "82784d59168f5d7c14e99c36e596e80888d6eccb"  # crtfc_key
     __operating_profit_filter = False # 영업이익 적자 필터
     __net_income_filter = False # 당기순이익 적자 필터
-    __dict_tickers = {}
     __base_time = datetime.today()
 
     def __init__(self):
         print("start to get ticker list : ", datetime.now())
         cur_folder_path = os.path.dirname(os.path.realpath(__file__))
-        cache_file_path = cur_folder_path + "/ticker_cache.txt"
+        #cache_file_path = cur_folder_path + "/ticker_cache.txt"
 
         ## 전날짜기준, 주말은 금요일 기준
         weekday = datetime.today().weekday()
@@ -36,34 +35,35 @@ class OpenDartManager:
             today = today - timedelta(days=3)
         else:
             today = today - timedelta(days=1)
+
         self.__base_time = today.strftime("%Y%m%d")
 
-        if os.path.exists(cache_file_path):
-            cache_file = open(cache_file_path, "rt")
-            lines = cache_file.readlines()
-            cache_file.close()
+        # if os.path.exists(cache_file_path):
+        #     cache_file = open(cache_file_path, "rt")
+        #     lines = cache_file.readlines()
+        #     cache_file.close()
+        #
+        #     for line in lines:
+        #         line = line.replace('\n','')
+        #         name_and_code = line.split(':');
+        #         self.__dict_tickers[name_and_code[0]] = name_and_code[1]
+        # else:
+        #     cache_file = open(cache_file_path, "wt")
+        #
+        #     tickers = stock.get_market_ticker_list(self.__base_time, market="KOSDAQ")
+        #     for ticker in tickers:
+        #         name = stock.get_market_ticker_name(ticker)
+        #         self.__dict_tickers[name] = ticker
+        #         cache_file.write(name + ":" + ticker + "\n")
+        #
+        #     tickers = stock.get_market_ticker_list(self.__base_time, market="KOSPI")
+        #     for ticker in tickers:
+        #         name = stock.get_market_ticker_name(ticker)
+        #         self.__dict_tickers[name] = ticker
+        #         cache_file.write(name + ":" + ticker + "\n")
+        #     cache_file.close()
 
-            for line in lines:
-                line = line.replace('\n','')
-                name_and_code = line.split(':');
-                self.__dict_tickers[name_and_code[0]] = name_and_code[1]
-        else:
-            cache_file = open(cache_file_path, "wt")
-
-            tickers = stock.get_market_ticker_list(self.__base_time, market="KOSDAQ")
-            for ticker in tickers:
-                name = stock.get_market_ticker_name(ticker)
-                self.__dict_tickers[name] = ticker
-                cache_file.write(name + ":" + ticker + "\n")
-
-            tickers = stock.get_market_ticker_list(self.__base_time, market="KOSPI")
-            for ticker in tickers:
-                name = stock.get_market_ticker_name(ticker)
-                self.__dict_tickers[name] = ticker
-                cache_file.write(name + ":" + ticker + "\n")
-            cache_file.close()
-
-        print("end to get ticker list : ", datetime.now())
+        #print("end to get ticker list : ", datetime.now())
 
     # 실적 필터링 설정
     def set_filter(self, operating_profit_filter, net_income_filter):
@@ -117,13 +117,13 @@ class OpenDartManager:
             cache_file = open(cache_file_path, "rt")
             lines = cache_file.readlines()
             cache_file.close()
-
+            ##TODO cache 파일에 읽기 -> #todo::추후에 DB로 변경
             print("corp_code_list cache start : ", datetime.now())
             corp_code_list = {}
             for line in lines:
                 line = line.replace('\n','')
-                name_and_code = line.split(':');
-                corp_code_list[name_and_code[0]] = name_and_code[1]
+                name_and_code = line.split(',');
+                corp_code_list[name_and_code[0]] = {name_and_code[1],name_and_code[2]}
             print("corp_code_list cache end : ", datetime.now())
             return corp_code_list
 
@@ -147,8 +147,10 @@ class OpenDartManager:
                     corp_code = list_tag.find("corp_code").text
                     if self.__is_stock_listing_corp(corp_code):
                         corp_name = list_tag.find("corp_name").text
+                        stock_code = list_tag.find("stock_code").text
                         corp_code_list[corp_name] = corp_code
-                        cache_file.write(corp_name + ":" + corp_code + "\n")
+                        ##TODO 파일에 쓰기 -> 추후에 DB로 변경
+                        cache_file.write(corp_name + "," + corp_code + "," + stock_code + "\n")
 
             cache_file.close()
             print("corp_code_list end : ", datetime.now())
@@ -158,31 +160,29 @@ class OpenDartManager:
     # ret : string
     def __get_report_code(self, quarter):
         print("__getReportCode")
-        if quarter == 1:
+        if quarter == "1":
             return "11013"  # 1분기 보고서
-        elif quarter == 2:
+        elif quarter == "2":
             return "11012"  # 2분기 보고서
-        elif quarter == 3:
+        elif quarter == "3":
             return "11014"  # 3분기 보고서
-        elif quarter == 4:
+        elif quarter == "4":
             return "11011"  # 4분기 보고서
         else:
             return
 
     # 기업 시총, 상장주식수 가져오기
     # ret : list [시가총액, 상장주식수]
-    def __get_stock_info(self, company_name):
+    def __get_stock_info(self, stock_code):
         try:
             ## 기업명으로 오늘 날짜 기준 시가총액, 상장주식수  가져오기
             # 1. 해당 기업명의 네이버 주식코드(티커) 알아오기
             if company_name not in self.__dict_tickers:
                 return []
 
-            ticker = self.__dict_tickers[company_name]
-
             # 2. df = stock.get_market_cap_by_date("20190101", "20190131", "005930") 호출
             #   df = 날짜, 시가총액, 거래량, 거래대금, 상장주식수
-            stock_df = stock.get_market_cap_by_date(self.__base_time, self.__base_time, ticker)
+            stock_df = stock.get_market_cap_by_date(self.__base_time, self.__base_time, stock_code)
 
             # 3. df에서 시가총액 컬럼 series
             series_cap = stock_df.get("시가총액")
@@ -209,45 +209,49 @@ class OpenDartManager:
     # 연결 재무제표 요청
     # ret : list [회사명, 매출액, 영업이익, 당기순이익]
     def __get_performance(self, corp_code, bsns_year, reprt_code):
+        try:
+            url = "https://opendart.fss.or.kr/api/fnlttSinglAcntAll.json?"
+            params = {'crtfc_key': self.__crtfcKey,
+                      'corp_code': corp_code,
+                      'bsns_year': bsns_year,
+                      'reprt_code': reprt_code,
+                      'fs_div': "OFS"}  # OFS : 재무제표, CFS : 연결재무제표
+            response = requests.get(url, params)
+            json_dict = json.loads(response.text)
+            data = []
+            num_of_data_to_find = 3  # 매출액, 영업이익, 당기순이익만 추출한다.
 
-        url = "https://opendart.fss.or.kr/api/fnlttSinglAcntAll.json?"
-        params = {'crtfc_key': self.__crtfcKey,
-                  'corp_code': corp_code,
-                  'bsns_year': str(bsns_year),
-                  'reprt_code': reprt_code,
-                  'fs_div': "OFS"}  # OFS : 재무제표, CFS : 연결재무제표
-        response = requests.get(url, params)
-        json_dict = json.loads(response.text)
-        data = []
-        num_of_data_to_find = 3  # 매출액, 영업이익, 당기순이익만 추출한다.
+            if json_dict['status'] == "000":
+                # 재무제표가 없을 수 있음
+                if 'list' in json_dict:
+                    # data list convert
+                    for line in json_dict['list']:
+                        if line['sj_div'] == 'CIS':  # 포괄손익계산서만 필요함
+                            if line['account_nm'] in '수익(매출액)' or line['account_nm'] in '영업수익' or \
+                                    line['account_nm'] in '영업이익(손실)' or line['account_nm'] in '당기순이익(손실)':
 
-        if json_dict['status'] == "000":
-            # 재무제표가 없을 수 있음
-            if 'list' in json_dict:
-                # data list convert
-                for line in json_dict['list']:
-                    if line['sj_div'] == 'CIS':  # 포괄손익계산서만 필요함
-                        if line['account_nm'] in '수익(매출액)' or line['account_nm'] in '영업수익' or \
-                                line['account_nm'] in '영업이익(손실)' or line['account_nm'] in '당기순이익(손실)':
+                                amount_data = 0
+                                if line['thstrm_amount'] != '':
+                                    amount_of_money = int(line['thstrm_amount'])
+                                    amount_of_money = round(amount_of_money / 100000000, 2)  # 억단위로 변환
 
-                            amount_data = 0
-                            if line['thstrm_amount'] != '':
-                                amount_of_money = int(line['thstrm_amount'])
-                                amount_of_money = round(amount_of_money / 100000000, 2)  # 억단위로 변환
+                                    if self.__check_filter(line['account_nm'], amount_of_money) is True:
+                                        return []
+                                    amount_data = amount_of_money
 
-                                if self.__check_filter(line['account_nm'], amount_of_money) is True:
-                                    return []
-                                amount_data = amount_of_money
+                                data.append(amount_data)
 
-                            data.append(amount_data)
+                                if data.__len__() == num_of_data_to_find:  # 이미 다 찾았으면 그만 찾아
+                                    return data
 
-                            if data.__len__() == num_of_data_to_find:  # 이미 다 찾았으면 그만 찾아
-                                return data
+            if data.__len__() < num_of_data_to_find:
+                return []
 
-        if data.__len__() < num_of_data_to_find:
+            return data
+
+        except:
+            print("[__get_performance] ", corp_code, " except")
             return []
-
-        return data
 
     # 연도 - 분기에 해당하는 실적 보고서 데이터 가져오기
     # ret : DataFrame
@@ -270,20 +274,20 @@ class OpenDartManager:
         url = "https://opendart.fss.or.kr/api/fnlttSinglAcntAll.json?"
         performance_table = []
 
-        for company_name, corp_code in corp_code_list.items():
+        for company_name, corp_code, stock_code in corp_code_list.items():
 
             company_data = []
             company_data.append(company_name)
 
             # 각 회사별 실적 데이터 가져오기
             performance_data = []   #'매출액(억원)', '영업이익(억원)', '당기순이익(억원)'
-            performance_data = self.__get_performance(corp_code, str(bsns_year), reprt_code)
+            performance_data = self.__get_performance(corp_code, bsns_year, reprt_code)
             if not performance_data:    #data empty
                 continue
 
             ## 시가총액, 상장주식수
             stock_data = []     #'시가총액', '상장주식수'
-            stock_data = self.__get_stock_info(company_name)
+            stock_data = self.__get_stock_info(stock_code)
             if not stock_data:    #data empty  #Todo:유가증권 이름과 네이버 증권 이름이 다른 경우가 있다.(ex 네이버증권 : 삼영전자 -> DART : 삼영전자공업)
                 continue
 
@@ -291,9 +295,12 @@ class OpenDartManager:
 
 
             # 개인적인 계산
-            # 멀티플 = 시가총액 / (당기 순이익 * 4)
+            # 멀티플 = 시가총액 / 당기 순이익
             a = float(stock_data[0])
-            b = float(performance_data[2]) * 4
+            b = float(performance_data[2])
+            # 사업보고서가 아닌 경우, 분기실적 * 4를 함
+            if quarter != "4":
+                b = b * 4
             multiple = round(a / b, 1)
             #multiple = int(stock_data[0]) / (int(float(performance_data[2])) * 4)
             multiple_data = []
@@ -309,6 +316,60 @@ class OpenDartManager:
         df = pd.DataFrame(performance_table, columns=column_names)
         return df
 
+    # date:YYYYMMDD
+    def get_report(self, date):
+        print('get_report_list')
+        rcept_no_list = {}
+
+        if date == '':
+            now = datetime.datetime.now()  # 2015-04-19 12:11:32.669083
+            date = now.strftime('%Y%m%d')
+
+        corp_code_list = self.__get_corp_code_list()
+
+        for company_name, corp_code in corp_code_list.items():
+
+            params = {
+                        'crtfc_key': "82784d59168f5d7c14e99c36e596e80888d6eccb",
+                        'last_reprt_at': 'Y',
+                        'corp_code': corp_code,
+                        'bgn_de': date,
+                        'pblntf_ty': 'I',
+                        'pblntf_detail_ty': 'I002',
+                        'sort': 'rpt',
+                        'corp_cls': 'Y'}
+            url = "https://opendart.fss.or.kr/api/list.json"
+
+            resonse = requests.get(url, params=params)
+            json_dict = json.loads(resonse.text)
+            if json_dict['status'] == "000":
+                if 'list' in json_dict:
+                    # data list convert
+                    find_rcept = False
+                    for data in json_dict['list']:
+                        if data['report_nm'] in '연결재무제표기준영업(잠정)실적':
+                            rcept_no_list[corp_code] = data['rcept_no']
+                            break
+
+
+        print(rcept_no_list)
+
+        for rcept_no in rcept_no_list:
+            params = {
+                'crtfc_key': "82784d59168f5d7c14e99c36e596e80888d6eccb",
+                'rcept_no': rcept_no
+            }
+            url = "https://opendart.fss.or.kr/api/document.xml"
+
+            resonse = requests.get(url, params=params)
+            json_dict = json.loads(resonse.text)
+            if json_dict['status'] == "000":
+                with ZipFile(BytesIO(response.content)) as zipfile:
+                    #file_list = zipfile.namelist()
+                    zipfile.extractall()
+
+
+
 
 #########################################################################################
 
@@ -319,27 +380,5 @@ class OpenDartManager:
 # 공시 리포트 리스트 검색
 # https: // opendart.fss. or.kr / api / list.json
 # date:YYYYMMDD
-# pblntf_detail_ty : H002 사업/반기/분기보고서
+# pblntf_detail_ty : I002
 ##todo:: 테스트 진행중인 api..
-def get_report_num_list_by_date(date):
-    print('get_report_list')
-    rcept_no_list = []
-    if date == '':
-        now = datetime.datetime.now()  # 2015-04-19 12:11:32.669083
-        date = now.strftime('%Y%m%d')
-    params = {'crtfc_key': "82784d59168f5d7c14e99c36e596e80888d6eccb",
-              'last_reprt_at': 'Y',
-              'bgn_de': date,
-              'pblntf_detail_ty': 'H002',
-              'corp_cls': 'Y'}
-    url = "https://opendart.fss.or.kr/api/list.json"
-
-    resonse = requests.get(url, params=params)
-    json_dict = json.loads(resonse.text)
-    if json_dict['status'] == "000":
-        if 'list' in json_dict:
-            # data list convert
-            for line in json_dict['list']:
-                rcept_no_list.append(line['rcept_no'])
-    print(rcept_no_list)
-    return rcept_no_list
